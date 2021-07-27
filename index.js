d60af671845bfa64483bb1fff396d631eab2a73f
bender-meemoo <esmith167@gmail.com>
2021-07-27 12:28:44 -0400

    Took out troubleshooting console.logs and fixed decrement button to not go below zero. Next is to make it responsive.

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-let saveEl = document.getElementById("save-el")
-const countEl = document.getElementById("count-el")
-const deleteBtn = document.getElementById("delete-btn")
+
+let saveEl = document.getElementById("save-el");
+const countEl = document.getElementById("count-el");
+const deleteBtn = document.getElementById("delete-btn");
 let count = 0;
 let countStr =  [];
 let time = 0;
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+    count += 1;
+    countEl.textContent = count;
 }
 
 function decrement() {
-    count -= 1
-    countEl.textContent = count
+    if(count > 0) {
+        count -= 1;
+    }
+    countEl.textContent = count;
 }
 
 function getTime() {
     let today = new Date();
-    time = today.getHours() + ":" + today.getMinutes()
+    time = today.getHours() + ":" + today.getMinutes();
     return time;
 }
 
 function render() {
-    countStr.push( {countSum: count, timeStamp: getTime(time) })
-    localStorage.setItem("countSum", JSON.stringify(countStr), "timeStamp", JSON.stringify(countStr))
-    console.log(localStorage.getItem("countSum"))
+    countStr.push( {countSum: count, timeStamp: getTime(time) });
+    localStorage.setItem("countSum", JSON.stringify(countStr), "timeStamp", JSON.stringify(countStr));
     let listEntries = "";
     for( let i = 0;  i <countStr.length; i++) {
         listEntries +=
         `
             <li> ${countStr[i].countSum} (${countStr[i].timeStamp})</li>
         `
-    }
+    };
 
-    saveEl.innerHTML = listEntries
-    console.log(countStr)
-    countEl.innerHTML = 0
-    count = 0
-}
+    saveEl.innerHTML = listEntries;
+    countEl.innerHTML = 0;
+    count = 0;
+};
 
 deleteBtn.addEventListener("dblclick", function(){
     saveEl.innerHTML = "";
-    console.log(saveEl)
-})
-
-console.log(getTime())
+    localStorage.clear();
+    countStr = [];
+});
